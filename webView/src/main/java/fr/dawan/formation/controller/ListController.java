da7fd05c46a785d38f8742ddd99879d2e7dbb05d
Jonathan Yvert <yvert.jonathan@hotmail.fr>
2020-02-04 17:14:15 +0100

    Updated ListController.java, listing.jsp and pom.xml.

--- webView/src/main/java/fr/dawan/formation/controller/ListController.java
+++ webView/src/main/java/fr/dawan/formation/controller/ListController.java
@@ -6,20 +6,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import fr.dawan.formation.modele.entities.User;
 import fr.dawan.formation.services.UserService;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/users")
 public class ListController extends HttpServlet {
+
+  private Gson gson = new Gson();
+
+  // Fonction qui envoie l'objet JSON correspond à l'adresse localhost:9090/webView/users
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     UserService userService = new UserService();
     List<User> users = userService.listing();
 
     request.setAttribute("users", users);
-    request.getRequestDispatcher("listing.jsp").forward(request, response);
+//    request.getRequestDispatcher("/WEB-INF/listing.jsp").forward(request, response); /* Avec tomcat7 mais ne fonctionne pas */
+
+    String usersJsonString = this.gson.toJson(users);
+    System.out.println(users);
+
+    PrintWriter out = response.getWriter();
+
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+
+    out.print(usersJsonString);
+    out.flush();
+
   }
 }
--- webView/src/main/webapp/WEB-INF/listing.jsp
+++ webView/src/main/webapp/WEB-INF/listing.jsp
@@ -1,6 +1,7 @@
 <!-- Les fichiers enregistré dans WEB-INF sont seulement accessible par les servlets -->
 
-<% page pageEncoding="UTF-8"%>
+<%@ page pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <!DOCTYPE html>
 <html>
@@ -11,6 +12,12 @@
 
     <body>
         <h1>Mes utilisateurs</h1>
-
+        <ul>
+            <c:forEach items="${users}" var="element">
+                <li>
+                    ${element.firstName} ${element.lastName}
+                </li>
+            </c:forEach>
+        </ul>
     </body>
 </html>
